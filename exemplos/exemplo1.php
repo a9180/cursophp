1e8bfe91ee02eeff30faf94e6cb3248fac98b8d4
a9180 <santos888venancio@gmail.com>
2024-09-13 11:09:59 -0300

    first commit

---
+++ exemplos/exemplo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo simples</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php 
+        $nome = "André Venâncio dos Santos";
+        echo "<p>Eu me chamo $nome.</p>";
+    ?>
+</body>
+</html>
