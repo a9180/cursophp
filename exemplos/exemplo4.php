6c9ab38ccf8d0e42570d10413264bb897b3301df
a9180 <santos888venancio@gmail.com>
2024-09-14 10:53:35 -0300

    Tudo atualizado

---
+++ exemplos/exemplo4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulação de strings com PHP</title>
+</head>
+<body>
+    <h1>Strings em PHP</h1>
+    <?php 
+        //$nome = 'André';
+       //$sobrenome = 'Venâncio dos Santos';
+        //echo "Olá $nome $sobrenome! \u{1F596}";
+
+        //const ESTADO = "MT";
+        //echo "Moro no " . ESTADO;
+
+        const CANAL = "Curso em Video \u{1F499}";
+        echo "Eu adoro esse " .CANAL;
+        echo "Estamos no ano de " . date('Y');
+    ?>
+</body>
+</html>
