985ac924112ea2e246377b3deda67ed7da802f45
a9180 <santos888venancio@gmail.com>
2024-09-15 11:10:13 -0300

    Atualizado

---
+++ Desafios/d001.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Antcessor e Sucessor</title>
+</head>
+<body>
+    <header>
+        <h1></h1>
+    </header>
+    <section>
+        <?php 
+            $numero = 1000;
+            $antecessor = $numero - 1;
+            $sucessor = $numero + 1;
+            echo ("O antecessor e o sucessor de $numero é $antecessor e $sucessor.");
+        ?>
+    </section>
+</body>
+</html>
