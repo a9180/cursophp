d3f35bdac549dfe7059acfaa732201afc8d3bb2a
a9180 <santos888venancio@gmail.com>
2024-09-15 07:27:55 -0300

    Tudo atualizado

---
+++ exemplos/exemplo6.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções aritméticas do PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Outras operações aritméticas</h1>
+    </header>
+    <section>
+        <?php 
+            //$r = abs(-2000);
+            //print ("A resposta é $r");
+
+            //$res = base_convert(1011, 2, 10);
+            //print ("A respota é $res");
+
+            //$res = sqrt(81) ; 
+           //print "A resposta é $res";
+
+           $r = 27 ** (1/3);
+            print("A resposta é $r");
+        ?>
+    </section>
+</body>
+</html>
