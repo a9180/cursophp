7bcd0f0865e221b9d1cff9aeffb77f54fda4deaf
a9180 <santos888venancio@gmail.com>
2024-09-15 18:40:32 -0300

    Um simples ajusti

--- Desafios/D001/d001.php
+++ Desafios/D001/d001.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="D001/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Antcessor e Sucessor</title>
 </head>
 <body>
