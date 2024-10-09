cc2f055b1b9c73ddb134da6df61aab1d8ca6d29c
a9180 <santos888venancio@gmail.com>
2024-10-09 18:14:12 -0400

    Tudo atualizado

--- Desafios/D003/conversor.php
+++ Desafios/D003/conversor.php
@@ -5,8 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Conversor de Moedas</h1>
+    </header>
+    <main>
+         <?php 
+            $cotação = 5.17;
+            $real = 1000;
+            $dólar =$real / $cotação;
+         ?>
+    </main>
 </body>
 </html>
